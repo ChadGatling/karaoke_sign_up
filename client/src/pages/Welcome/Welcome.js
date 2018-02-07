20abdd7a3130f48e33cb769be05ebe50c48a3518
Elliott Wendel <ewendel88@gmail.com>
2018-02-07 15:12:48 -0600

    bit of front end css

--- client/src/pages/Welcome/Welcome.js
+++ client/src/pages/Welcome/Welcome.js
@@ -11,6 +11,15 @@ var Style = {
   // backgroundImage: "url(https://randolphsbilliards.com/wp-content/uploads/2016/05/Facebook-Event-Background.png)"
 };
 
+var margin = {
+	margin: "0px 10px",
+};
+
+var center = {
+	margin: "auto",
+	width: "50%"
+};
+
 class Welcome extends Component {
 	state = {
 		username: ""
@@ -45,10 +54,10 @@ class Welcome extends Component {
 			<div style={ Style }>
 				<Nav username={this.state.username}/>
 				<h1>Welcome</h1>
-				<p>Ready to sing? Just sing up or sign in, request a song, and have a drink.</p>
+				<p style={margin}>Ready to sing? Just sing up or sign in, request a song, and have a drink.</p>
 				<div>
 					{!this.state.username &&
-						<form>
+						<form style={center}>
 							<FormBtn onClick={this.handleSignUp}>
 								Register
 							</FormBtn>
@@ -57,6 +66,15 @@ class Welcome extends Component {
 							</FormBtn>						
 						</form>
 					}
+				</div>
+				<br/>
+				<div style={margin}>
+					<p>
+						Karaoke-Sign-Up is intended to be an app for users to sign up to karaoke events from their table. At this point functionality exists for users to register, log in, request a song, and log out; as well as navigating pages and viewing the current list of singers.
+					</p>
+					<p>
+						My intention is to allow users to pick from a list of available locations and confirm with gps data. There will be a separate area for use by the DJ himself were he can see the order of signups and confirm that a song is available. I am also interested in getting the location's songs so as to automatically confirm a song's availability. I would like to be able to determine if a user has left the location and automatically remove that user from the upcoming singers list.
+					</p>
 				</div>
 			</div>
 		);
